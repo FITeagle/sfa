b11577c497229cbf07c704efaafb5ad117fd41e1
mitjanikolaus <mitjanikolaus@gmail.com>
2015-01-17 18:30:57 +0100

    renamed partOfGroup in partOfFederation

--- src/main/java/org/fiteagle/north/sfa/am/dm/SFA_AM_MDBSender.java
+++ src/main/java/org/fiteagle/north/sfa/am/dm/SFA_AM_MDBSender.java
@@ -77,8 +77,8 @@ public class SFA_AM_MDBSender {
     String query = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
         + "PREFIX omn: <http://open-multinet.info/ontology/omn#> "
         + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " + "CONSTRUCT { "
-        + "?resource omn:partOfGroup ?testbed. " + "?resource rdf:type ?type. }" + "FROM " + TRIPLET_STORE_URL
-        + "WHERE {" + "?resource omn:partOfGroup ?testbed. " + "?testbed a omn:Testbed. "
+        + "?resource omn:partOfFederation ?testbed. " + "?resource rdf:type ?type. }" + "FROM " + TRIPLET_STORE_URL
+        + "WHERE {" + "?resource omn:partOfFederation ?testbed. " + "?testbed a omn:Testbed. "
         + "OPTIONAL {?resource rdf:type ?type. } }";
     Model resultModel = sendSPARQLQueryRequest(query, IMessageBus.TARGET_RESOURCE_ADAPTER_MANAGER);
     
@@ -128,10 +128,10 @@ public class SFA_AM_MDBSender {
           + "PREFIX omn: <http://open-multinet.info/ontology/omn#> "
           + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> "
           + "PREFIX wgs: <http://www.w3.org/2003/01/geo/wgs84_pos#> " + "CONSTRUCT { "
-          + "?resource omn:partOfGroup ?testbed." + "?resource rdfs:label ?label. "
+          + "?resource omn:partOfFederation ?testbed." + "?resource rdfs:label ?label. "
           + "?resource rdfs:comment ?comment." + "?resource rdf:type ?type. " + "?resource wgs:lat ?lat. "
           + "?resource wgs:long ?long. } " + "FROM " + TRIPLET_STORE_URL + "WHERE {"
-          + "?resource omn:partOfGroup ?testbed. " + "?testbed a omn:Testbed. "
+          + "?resource omn:partOfFederation ?testbed. " + "?testbed a omn:Testbed. "
           + "OPTIONAL {?resource rdfs:label ?label. }" + "OPTIONAL {?resource rdfs:comment ?comment. }"
           + "OPTIONAL {?resource rdf:type ?type. }" + "OPTIONAL {?resource wgs:lat ?lat. }"
           + "OPTIONAL {?resource wgs:long ?long. } }";
