ea0e28c5b1aeb93e102fb34fab4beb542b8994de
Alex-O. <wpinf05.ortlieb@gmail.com>
2015-07-16 17:15:46 +0200

    Timer class now implemented

--- src/main/java/org/fiteagle/north/sfa/StartUp.java
+++ src/main/java/org/fiteagle/north/sfa/StartUp.java
@@ -1,62 +1,76 @@
 package org.fiteagle.north.sfa;
 
 import info.openmultinet.ontology.exceptions.InvalidModelException;
-import info.openmultinet.ontology.vocabulary.Omn_component;
-import info.openmultinet.ontology.vocabulary.Omn_federation;
 import info.openmultinet.ontology.vocabulary.Omn_resource;
-import info.openmultinet.ontology.vocabulary.Omn_service;
+
+import java.util.concurrent.Callable;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
-import org.apache.jena.atlas.logging.Log;
-import org.fiteagle.api.core.IMessageBus;
-import org.fiteagle.api.core.OntologyModelUtil;
+import org.fiteagle.api.core.TimerHelper;
 import org.fiteagle.core.tripletStoreAccessor.TripletStoreAccessor;
 import org.fiteagle.core.tripletStoreAccessor.TripletStoreAccessor.ResourceRepositoryException;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
-import com.hp.hpl.jena.vocabulary.RDFS;
-
 
 @Startup
 @Singleton
 public class StartUp {
 
-private Model defaultModel;	
-	
+	Model defaultModel;
+
 	@PostConstruct
-    public void addSfaApi(){
-    	Model model = ModelFactory.createDefaultModel();
-    Resource resource = model.createResource("http://localhost/resource/SFA");
-    resource.addProperty(Omn_resource.hasInterface, "/sfa/api/am/v3");
-    resource.addProperty(Omn_resource.hasInterface, "/sfa/api/sa/v1");
-    try {
-		TripletStoreAccessor.addResource(resource);
-		defaultModel = model;
-	} catch (ResourceRepositoryException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	public void addSfaApi() {
+		setDefaultModel();
+		TimerHelper timer = new TimerHelper(new SfaAPI());
 	}
-    }
-	
 	
 	@PreDestroy
-	public void deleteSfaApi() {
-		try{	
-	    TripletStoreAccessor.deleteModel(defaultModel);
-		} catch (ResourceRepositoryException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidModelException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
+	public void deleteNativeApi() {
+		TimerHelper timer = new TimerHelper(new DeleteSfaAPI());
 	}
+
+	private Model setDefaultModel() {
+		Model model = ModelFactory.createDefaultModel();
+		Resource resource = model
+				.createResource("http://localhost/resource/SFA");
+		resource.addProperty(Omn_resource.hasInterface, "/sfa/api/am/v3");
+		resource.addProperty(Omn_resource.hasInterface, "/sfa/api/sa/v1");
+		defaultModel = model;
+
+		return model;
+	}
+
+	final class DeleteSfaAPI implements Callable<Void> {
+
+		@Override
+		public Void call() throws ResourceRepositoryException, InvalidModelException {
+			if (defaultModel == null) {
+				TripletStoreAccessor.deleteModel(setDefaultModel());
+			} else {
+				TripletStoreAccessor.deleteModel(defaultModel);
+			}
+			return null;
+		}
+	}
+	
+	class SfaAPI implements Callable<Void> {
+
+		@Override
+		public Void call() throws ResourceRepositoryException {
+			if (defaultModel == null) {
+				TripletStoreAccessor.addResource(setDefaultModel().getResource(
+						"http://localhost/resource/SFA"));
+			} else {
+				TripletStoreAccessor.addResource(defaultModel
+						.getResource("http://localhost/resource/SFA"));
+			}
+			return null;
+		}
+}
 }
