85d9da957c6ebf3a2a68a8cb06a795dc23539280
Alex-O. <wpinf05.ortlieb@gmail.com>
2015-07-16 17:30:54 +0200

    uncommented PreDestroy

--- src/main/java/org/fiteagle/north/sfa/StartUp.java
+++ src/main/java/org/fiteagle/north/sfa/StartUp.java
@@ -30,10 +30,10 @@ public class StartUp {
 		TimerHelper timer = new TimerHelper(new SfaAPI());
 	}
 	
-	@PreDestroy
-	public void deleteNativeApi() {
-		TimerHelper timer = new TimerHelper(new DeleteSfaAPI());
-	}
+//	@PreDestroy
+//	public void deleteNativeApi() {
+//		TimerHelper timer = new TimerHelper(new DeleteSfaAPI());
+//	}
 
 	private Model setDefaultModel() {
 		Model model = ModelFactory.createDefaultModel();
