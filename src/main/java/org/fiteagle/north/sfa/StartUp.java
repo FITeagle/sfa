f779a5d396953d28898423a66bc13e9ef80d11d3
Alex-O. <wpinf05.ortlieb@gmail.com>
2015-07-21 18:01:33 +0200

    Sfa is now using TimerSevice instead of Threads and TimerTasks

--- src/main/java/org/fiteagle/north/sfa/StartUp.java
+++ src/main/java/org/fiteagle/north/sfa/StartUp.java
@@ -1,16 +1,20 @@
 package org.fiteagle.north.sfa;
 
-import info.openmultinet.ontology.exceptions.InvalidModelException;
 import info.openmultinet.ontology.vocabulary.Omn_resource;
 
-import java.util.concurrent.Callable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.ejb.Timeout;
+import javax.ejb.Timer;
+import javax.ejb.TimerConfig;
+import javax.ejb.TimerService;
 
-import org.fiteagle.api.core.TimerHelper;
+import org.apache.jena.atlas.web.HttpException;
 import org.fiteagle.api.tripletStoreAccessor.TripletStoreAccessor;
 import org.fiteagle.api.tripletStoreAccessor.TripletStoreAccessor.ResourceRepositoryException;
 
@@ -22,55 +26,60 @@ import com.hp.hpl.jena.rdf.model.Resource;
 @Singleton
 public class StartUp {
 
+	private static final Logger LOGGER = Logger.getLogger(StartUp.class
+			.getName());
+
+	@javax.annotation.Resource
+	private TimerService timerService;
+
 	Model defaultModel;
+	private int failureCounter = 0;
+	private static String resourceUri = "http://localhost/resource/SFA"; 
 
 	@PostConstruct
 	public void addSfaApi() {
 		setDefaultModel();
-		TimerHelper timer = new TimerHelper(new SfaAPI());
+		timerService.createIntervalTimer(0, 5000, new TimerConfig());
 	}
-	
-//	@PreDestroy
-//	public void deleteNativeApi() {
-//		TimerHelper timer = new TimerHelper(new DeleteSfaAPI());
-//	}
+
+	@PreDestroy
+	public void deleteSfaApi() {
+
+	}
+
+	@Timeout
+	public void timerMethod(Timer timer) {
+		if(failureCounter < 10){
+			try {
+				if (defaultModel == null) {
+					TripletStoreAccessor.addResource(setDefaultModel().getResource(
+							resourceUri));
+					timer.cancel();
+				} else {
+					TripletStoreAccessor.addResource(defaultModel
+							.getResource(resourceUri));
+					timer.cancel();
+				}
+			} catch (ResourceRepositoryException e) {
+				failureCounter++;
+			} catch (HttpException e) {
+				failureCounter++;
+			}	
+		}else{
+			 LOGGER.log(Level.SEVERE,
+			 "Tried to add something to Database several times, but failed. Please check the OpenRDF-Database");
+		}
+
+	}
 
 	private Model setDefaultModel() {
 		Model model = ModelFactory.createDefaultModel();
 		Resource resource = model
-				.createResource("http://localhost/resource/SFA");
+				.createResource(resourceUri);
 		resource.addProperty(Omn_resource.hasInterface, "/sfa/api/am/v3");
 		resource.addProperty(Omn_resource.hasInterface, "/sfa/api/sa/v1");
 		defaultModel = model;
 
 		return model;
 	}
-
-	final class DeleteSfaAPI implements Callable<Void> {
-
-		@Override
-		public Void call() throws ResourceRepositoryException, InvalidModelException {
-			if (defaultModel == null) {
-				TripletStoreAccessor.deleteModel(setDefaultModel());
-			} else {
-				TripletStoreAccessor.deleteModel(defaultModel);
-			}
-			return null;
-		}
-	}
-	
-	class SfaAPI implements Callable<Void> {
-
-		@Override
-		public Void call() throws ResourceRepositoryException {
-			if (defaultModel == null) {
-				TripletStoreAccessor.addResource(setDefaultModel().getResource(
-						"http://localhost/resource/SFA"));
-			} else {
-				TripletStoreAccessor.addResource(defaultModel
-						.getResource("http://localhost/resource/SFA"));
-			}
-			return null;
-		}
-}
 }
