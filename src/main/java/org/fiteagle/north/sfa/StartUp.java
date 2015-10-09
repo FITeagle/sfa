3861a5f14ce5fe2a3a4731ed2410179617d8e626
dnehls <daniel.nehls@tu-berlin.de>
2015-10-09 11:23:54 +0200

    relaxed timer

--- src/main/java/org/fiteagle/north/sfa/StartUp.java
+++ src/main/java/org/fiteagle/north/sfa/StartUp.java
@@ -26,64 +26,65 @@ import com.hp.hpl.jena.rdf.model.Resource;
 @Singleton
 public class StartUp {
 
-	private static final Logger LOGGER = Logger.getLogger(StartUp.class
-			.getName());
+    private static final Logger LOGGER = Logger.getLogger(StartUp.class
+            .getName());
 
-	@javax.annotation.Resource
-	private TimerService timerService;
+    @javax.annotation.Resource
+    private TimerService timerService;
 
-	Model defaultModel;
-	private int failureCounter = 0;
-	private static String resourceUri = "http://localhost/resource/SFA"; 
+    Model defaultModel;
+    private int failureCounter = 0;
+    private static String resourceUri = "http://localhost/resource/SFA";
 
-	@PostConstruct
-	public void addSfaApi() {
-		setDefaultModel();
-		timerService.createIntervalTimer(0, 5000, new TimerConfig());
-	}
+    @PostConstruct
+    public void addSfaApi() {
+        setDefaultModel();
+        timerService.createIntervalTimer(0, 5000, new TimerConfig());
+    }
 
-	@PreDestroy
-	public void deleteSfaApi() {
+    @PreDestroy
+    public void deleteSfaApi() {
 
-	}
+    }
 
-	@Timeout
-	public void timerMethod(Timer timer) {
-		if(failureCounter < 10){
-			try {
-				if (defaultModel == null) {
-					TripletStoreAccessor.addResource(setDefaultModel().getResource(
-							resourceUri));
-					timer.cancel();
-				} else {
-					TripletStoreAccessor.addResource(defaultModel
-							.getResource(resourceUri));
-					timer.cancel();
-				}
-			} catch (ResourceRepositoryException e) {
-				LOGGER.log(Level.INFO,
-						 "Errored while adding something to Database - will try again");
-				failureCounter++;
-			} catch (HttpException e) {
-				LOGGER.log(Level.INFO,
-						 "Couldn't find RDF Database - will try again");
-				failureCounter++;
-			}	
-		}else{
-			 LOGGER.log(Level.SEVERE,
-			 "Tried to add something to Database several times, but failed. Please check the OpenRDF-Database");
-		}
+    @Timeout
+    public void timerMethod(Timer timer) {
+        if(failureCounter < 100){
+            try {
+                if (defaultModel == null) {
+                    TripletStoreAccessor.addResource(setDefaultModel().getResource(
+                            resourceUri));
+                    timer.cancel();
+                } else {
+                    TripletStoreAccessor.addResource(defaultModel
+                            .getResource(resourceUri));
+                    timer.cancel();
+                }
+            } catch (ResourceRepositoryException e) {
+                LOGGER.log(Level.INFO,
+                        "Errored while adding something to Database - will try again");
+                failureCounter++;
+            } catch (HttpException e) {
+                LOGGER.log(Level.INFO,
+                        "Couldn't find RDF Database - will try again");
+                failureCounter++;
+            }
+        }else{
+            LOGGER.log(Level.SEVERE,
+                    "Tried to add something to Database several times, but failed. Please check the OpenRDF-Database");
+            timer.cancel();
+        }
 
-	}
+    }
 
-	private Model setDefaultModel() {
-		Model model = ModelFactory.createDefaultModel();
-		Resource resource = model
-				.createResource(resourceUri);
-		resource.addProperty(Omn_resource.hasInterface, "/sfa/api/am/v3");
-		resource.addProperty(Omn_resource.hasInterface, "/sfa/api/sa/v1");
-		defaultModel = model;
+    private Model setDefaultModel() {
+        Model model = ModelFactory.createDefaultModel();
+        Resource resource = model
+                .createResource(resourceUri);
+        resource.addProperty(Omn_resource.hasInterface, "/sfa/api/am/v3");
+        resource.addProperty(Omn_resource.hasInterface, "/sfa/api/sa/v1");
+        defaultModel = model;
 
-		return model;
-	}
+        return model;
+    }
 }
