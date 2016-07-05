ff8e42a7fb836e3656f764765dc6f4723592f571
Alex-O. <wpinf05.ortlieb@gmail.com>
2016-07-05 17:53:42 +0200

    sfa now giving back privat resources on ListResources(for openbaton)

--- src/main/java/org/fiteagle/north/sfa/am/listResources/ListResourcesProcessor.java
+++ src/main/java/org/fiteagle/north/sfa/am/listResources/ListResourcesProcessor.java
@@ -1,14 +1,18 @@
 package org.fiteagle.north.sfa.am.listResources;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 import javax.jms.JMSException;
 import javax.xml.bind.JAXBException;
 
+import org.apache.log4j.lf5.LogLevel;
 import org.fiteagle.api.core.Config;
 import org.fiteagle.api.core.IConfig;
 import org.fiteagle.api.core.IGeni;
@@ -18,11 +22,16 @@ import org.fiteagle.api.core.OntologyModelUtil;
 import org.fiteagle.north.sfa.am.ISFA_AM;
 import org.fiteagle.north.sfa.am.common.AbstractMethodProcessor;
 
+import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.rdf.model.AnonId;
+import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.ResIterator;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.vocabulary.RDF;
 
 import info.openmultinet.ontology.exceptions.InvalidModelException;
@@ -52,9 +61,9 @@ public class ListResourcesProcessor extends AbstractMethodProcessor{
         ResIterator resIterator = resourcesResult.listSubjectsWithProperty(Omn_lifecycle.canImplement);
         while(resIterator.hasNext()){
             Resource resource = resIterator.nextResource();
-            topology.addProperty(Omn.hasResource, resource);
-            resource.addProperty(Omn.isResourceOf, topology);
-            topologyModel.add(resource.getModel());
+                topology.addProperty(Omn.hasResource, resource);
+                resource.addProperty(Omn.isResourceOf, topology);
+                topologyModel.add(resource.getModel());	
         }
         this.addManagerId(topologyModel);
         return topologyModel;
@@ -65,6 +74,37 @@ public class ListResourcesProcessor extends AbstractMethodProcessor{
         String serializedModel = MessageUtil.serializeModel(requestModel, IMessageBus.SERIALIZATION_TURTLE);
         LOGGER.info("START: Listing resources: " + serializedModel);
         Model model = getSender().sendRDFRequest(serializedModel, IMessageBus.TYPE_GET, IMessageBus.TARGET_RESOURCE_ADAPTER_MANAGER);
+        ResIterator it = model.listSubjectsWithProperty(Omn.isAttributeOf);
+        while(it.hasNext()){
+        	Resource resource = it.next();
+        	Statement statement = resource.getProperty(Omn.isAttributeOf);
+        	RDFNode rdfObject = statement.getObject();
+        	Node node = rdfObject.asNode();
+        	String nodeString = node.getLiteralValue().toString();
+        	if(nodeString.equals("d28a8a82-d503-42c5-80e5-899469e9255d")){
+                LOGGER.info(resource.toString());
+        	}else{
+                LOGGER.info("RESOURCE NOT FOR THIS EXPERIMENTER");
+                List<Statement> statementList = new ArrayList<Statement>();
+                StmtIterator statementIterator = model.listStatements();
+                while(statementIterator.hasNext()){
+                	Statement s = statementIterator.next();
+                	if(s.getSubject().getURI().equals(resource.getURI())){
+                        LOGGER.info("Found Resource we have to delete" + s.getSubject().getURI());
+                        statementList.add(s);
+                	}
+                	if(s.getObject().toString().equals(resource.getURI())){
+                        LOGGER.info("Found Resource we have to delete" + s.getSubject().getURI());
+                        statementList.add(s);
+                	}
+
+
+                }
+                model.remove(statementList);
+
+        	}
+
+        }
         LOGGER.info("END: Listing resources: " + OntologyModelUtil.toString(model));
        return  model;
     }
@@ -84,6 +124,8 @@ public class ListResourcesProcessor extends AbstractMethodProcessor{
       if(ISFA_AM.VERSION_3.equalsIgnoreCase(this.delegate.getRspecVersion()) && ISFA_AM.GENI.equalsIgnoreCase(this.delegate.getRspecType())){
         AdvertisementConverter converter = new AdvertisementConverter();
           converter.setVerbose(true);
+          LOGGER.log(Level.SEVERE,topologyModel.toString());
+
         testbedResources = converter.getRSpec(topologyModel);
     }else if("omn".equals(this.delegate.getRspecType())){
          testbedResources = MessageUtil.serializeModel(topologyModel, IMessageBus.SERIALIZATION_RDFXML);
